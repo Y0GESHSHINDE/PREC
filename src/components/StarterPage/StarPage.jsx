2e79192dd7fcdd9b79db7752eeaaa5ece8717ef2
y0GESHSHINDE <yogeshshinde3624@gmail.com>
2024-10-15 06:05:25 +0530

    login page

---
+++ src/components/StarterPage/StarPage.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './StartPage.css'
+function StarPage() {
+   return (
+      <div className="row d-flex justify-content-center " id='mainDiv' >
+         <div className="col-10 col-sm-8 col-md-8 col-lg-6 col-xl-4  " id='FormDiv'>
+            <div className="card col-12 text-center ">
+               <div className=' card-body ' id='CardBd'>
+                  <form className='  ' action="ok">
+                     <h1>Login</h1>
+                     <input type="email" name="" id="" placeholder='Enter Your Email' className='form-control mb-4 mt-4' />
+                     <input type="email" name="" id="" placeholder='Enter Your PassWord' className='form-control mb-4' />
+                     <select class="form-select mb-4" aria-label="Default select example">
+                        <option selected>Class</option>
+                        <option value="1">SE</option>
+                        <option value="2">TE</option>
+                        <option value="3">BE</option>
+                     </select>
+                     <button class="btn btn-primary form-control w-25 mb-4" type="submit">Button</button>
+
+                  </form>
+               </div>
+            </div>
+         </div>
+      </div>
+   )
+}
+
+export default StarPage;
