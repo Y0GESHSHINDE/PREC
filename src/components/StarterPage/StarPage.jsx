9c175b49758aef3b4ad5d9f9986df990b8ef7030
Y0GESHSHINDE <yogeshshinde3624@gmail.com>
2024-10-15 19:38:03 +0530

    HeadingUpdate

--- src/components/StarterPage/StarPage.jsx
+++ src/components/StarterPage/StarPage.jsx
@@ -7,18 +7,19 @@ function StarPage() {
             <div className="card col-12 text-center ">
                <div className=' card-body ' id='CardBd'>
                   <form className='  ' action="ok">
-                     <h1>Login</h1>
+                     <h1>Wellcome to PREC</h1>
+                     <h3 className='mt-3'>Login</h3>
                      <input type="email" name="" id="" placeholder='Enter Your Email' className='form-control mb-4 mt-4' />
                      <input type="email" name="" id="" placeholder='Enter Your PassWord' className='form-control mb-4' />
                      <select class="form-select mb-4" aria-label="Default select example">
                         <option selected>Class</option>
                         <option value="1">SE</option>
                         <option value="2">TE</option>
-                        <option value="3">BE</option>
+                        <option value="3">BE</option>x
                      </select>
-                     <button class="btn btn-primary form-control w-25 mb-4" type="submit">Button</button>
-
-                  </form>
+                     <button class="btn btn-primary form-control w-25 mb-4 fs-4 " type="submit">login</button>
+                     <div><a href="">Forgot Password?</a></div>
+                     </form>
                </div>
             </div>
          </div>
